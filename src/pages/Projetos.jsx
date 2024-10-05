2b174b75ed79b8cd40f5ec9e32218c4baee69802
Clara <anaclaraguimaraesremotto@gmail.com>
2024-10-05 16:49:33 -0300

    rotas

---
+++ src/pages/Projetos.jsx
@@ -0,0 +1,8 @@
+export default function Projetos(){
+
+    return(
+        <>
+            <h1> PROJETOS</h1>
+        </>
+    )
+}
---
+++ src/pages/Sobre.jsx
@@ -0,0 +1,9 @@
+
+export default function Sobre(){
+
+        return(
+            <>
+                <h1> SOBRE</h1>
+            </>
+        )
+    }
